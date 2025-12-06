895f8889b8d919b57b4573f4dec2d47e7e28d3cb
lhuxohl <zeephyros@gmail.com>
2025-12-06 21:58:41 +0100

    initial commit

---
+++ src/main/kotlin/de/tivin/mobLootChest/MobLootChest.kt
@@ -0,0 +1,14 @@
+package de.tivin.mobLootChest
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class MobLootChest : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
